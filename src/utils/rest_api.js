00a603ca97ba469139778d551d33d813ff56a11e
Juan Rivillas <juan@viridis-solutions.com>
2017-09-04 16:21:03 -0300

    Removed error in restApi

--- src/utils/rest_api.js
+++ src/utils/rest_api.js
@@ -3,17 +3,11 @@ import $ from 'jquery';
 const BASE_REST_API = 'http://localhost:3000';
 
 export default class RestApi {
-
   DEFAULT_REST_PARAMS = {
     dataType: 'json'
   };
 
-  DEFAULT_HEADERS = {
-    'Autorization': `Bearer ${this.getToken()}`
-  };
-
   get(url, params) {
-    
     const requestParams = Object.assign({}, params, {
       url: `${BASE_REST_API}/${url}`,
       type: 'GET',
@@ -21,11 +15,9 @@ export default class RestApi {
     });
 
     return $.ajax(requestParams);
-
   }
 
   post(url, params, postData) {
-    
     const requestParams = Object.assign({}, params, {
       url: `${BASE_REST_API}/${url}`,
       type: 'POST',
@@ -34,7 +26,5 @@ export default class RestApi {
     });
 
     return $.ajax(requestParams);
-
   }
-
 }
